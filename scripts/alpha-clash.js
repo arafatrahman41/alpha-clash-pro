af96fd1f53aea5809e93a206e77ef0fdee4999fc
arafatrahman41 <arafatrahman41@outlook.com>
2024-04-03 12:10:56 +0600

    connect to the screen with keyboard

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -3,15 +3,29 @@
 //     const homeScreen = document.getElementById('home-screen').classList.add('hidden');
 //     // homeScreen.classList.add('hidden')
 
-//     /* show the playground */
+//     /*step-2: show the playground */
 //     const playGroundScreen = document.getElementById('play-ground').classList.remove('hidden');
 //     // playGroundScreen.classList.remove('hidden')
 
 //     // console.log(playGroundScreen.classList);
 // }
 
+function continueGame(){
+    //step-1: generate a random alphabet
+    const alphabet = getARandomAlphabet();
+    // console.log(alphabet);
+
+    // set random generated alphabet to the screen(show it);
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
+
+    //set background color
+    setBackgroundColorById(alphabet);
+}
+
 function play(){
     hideElementById('home-screen');
     showElementById('play-ground');
+    continueGame();
 }
 
